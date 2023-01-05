a7c79ba5583ef34088ab6778d18122491cbb4e33
Rohit <Rohit@spotblhol0128.awsspoton.com>
2023-01-05 17:44:04 +0530

    Added: UC1 analyze and response.

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,7 +1,15 @@
 package com.bridgelabz;
-
 public class MoodAnalyzer {
-    public static void main(String[] args) {
-        System.out.println("welcome to mood analyser program.");
+
+    private String message;
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        if(message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
